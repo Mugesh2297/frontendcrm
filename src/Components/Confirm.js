aaab72f1a10a571e14dd44d6affba6b4f2c5cc6c
Mugesh <mugeshkumark22@gmail.com>
2022-10-29 17:18:19 +0530

    final

--- src/Components/Confirm.js
+++ src/Components/Confirm.js
@@ -34,10 +34,8 @@ function Confirm() {
                 
             } catch (err) {
                console.log(err.response);
-                alert(err.response.data.msg);
-                if(err.response.status === 500){
-                    alert(err.response.data.Message);
-                }
+                alert(err.response.data.Message);
+                navigate("/")
             }
         }
     });
